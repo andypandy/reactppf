ca819111230f9fab107bbedc1c02454f0376f905
andypandy <andrew.samuelsen@gmail.com>
2016-03-30 11:42:16 -0700

    Add `Link`s to NavLinks

--- components/NavLinks.js
+++ components/NavLinks.js
@@ -1,11 +1,12 @@
 import React from 'react'
+var Link = require('react-router').Link
 
 const NavLinks = (props) => (
   <ul>
     <li>Constants</li>
     {console.log(props)}
     {props.properties.map((property)=>
-      <li key={property.id}>{property.name}</li>
+      <li key={property.property_id}><Link to={`/property/${property.property_id}`}>{property.name}</Link></li>
     )}
     <li>New property</li>
   </ul>
