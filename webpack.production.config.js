161dff260beb76e6f8740e2d66262b7b89943994
andypandy <andrew.samuelsen@gmail.com>
2016-06-09 10:38:27 -0700

    Setup for heroic

---
+++ webpack.production.config.js
@@ -0,0 +1,37 @@
+
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        minimize: true,
+        compress: {
+          warnings: false
+        }
+      })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel?presets[]=es2015',
+        exclude: /node_modules/,
+        include: __dirname
+      },
+      { test: /\.css$/, 
+        loader: "style-loader!css-loader" 
+      }
+    ]
+  }
+}
