e17f05ddf1ad5fdcdb38fbd0c542814840d233d8
andypandy <andrew.samuelsen@gmail.com>
2016-06-09 10:56:01 -0700

    Try new server

---
+++ server1.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const indexPath = path.join(__dirname, './index.html')
+const publicPath = express.static(path.join(__dirname, './static'))
+
+app.use('/static', publicPath)
+app.get('/', function (_, res) { res.sendFile(indexPath) })
+
+const port = (process.env.PORT || 8080)
+
+if (process.env.NODE_ENV !== 'production') {
+  const webpack = require('webpack')
+  const webpackDevMiddleware = require('webpack-dev-middleware')
+  const webpackHotMiddleware = require('webpack-hot-middleware')
+  const config = require('./webpack.config.js')
+  const compiler = webpack(config)
+
+  app.use(webpackHotMiddleware(compiler))
+  app.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+  }))
+}
+
+app.listen(port)
+console.log(`Listening at http://localhost:${port}`)
